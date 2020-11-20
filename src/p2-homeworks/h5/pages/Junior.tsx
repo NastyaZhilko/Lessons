2be2d933e9906029da8ac187e47b654f724d52e1
NastyaZhilko <nasten4ik2010@gmail.com>
2020-11-20 17:22:25 +0300

    second commit

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-/*import HW6 from "../../h6/HW6";
-import HW7 from "../../h7/HW7";
+import HW6 from "../../h6/HW6";
+/*import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
 */
@@ -10,7 +10,7 @@ function Junior() {
     return (
         <div>
 
-            {/*<HW6/>*/}
+            <HW6/>
             {/*<HW7/>*/}
             {/*<HW8/>*/}
             {/*<HW9/>*/}
