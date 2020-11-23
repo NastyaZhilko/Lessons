6d6c549daa79aa935046aa24a659455ec2d3c43d
NastyaZhilko <nasten4ik2010@gmail.com>
2020-11-23 21:29:02 +0300

    Updates

---
+++ precache-manifest.9535240aec1ad5e32a0609d684b9fff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f664682c0f884646d2ec99219a044f",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "7df57fd632c6ad5a7040",
+    "url": "/simple-react-homeworks-master/static/css/main.ccd27b0c.chunk.css"
+  },
+  {
+    "revision": "5824db7bcdd375a62dba",
+    "url": "/simple-react-homeworks-master/static/js/2.8eca5c41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks-master/static/js/2.8eca5c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df57fd632c6ad5a7040",
+    "url": "/simple-react-homeworks-master/static/js/main.bb203dcc.chunk.js"
+  },
+  {
+    "revision": "4d3b136f42fc51341e1d",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.39865212.js"
+  }
+]);
