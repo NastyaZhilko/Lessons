7bdb8998b8cb06577b07933b03a4023e3cf6f340
NastyaZhilko <nasten4ik2010@gmail.com>
2020-12-22 22:00:33 +0300

    Updates

---
+++ precache-manifest.f26eb1f4160078adaf366ab1d7250581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe12592fac401aa7c5aa184f2c27709",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "c8eee7bf026c874b5696",
+    "url": "/simple-react-homeworks-master/static/css/main.7d977766.chunk.css"
+  },
+  {
+    "revision": "c06b164f7474be657a76",
+    "url": "/simple-react-homeworks-master/static/js/2.78e00f13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks-master/static/js/2.78e00f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8eee7bf026c874b5696",
+    "url": "/simple-react-homeworks-master/static/js/main.45b97131.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  },
+  {
+    "revision": "0ad6a0ceb4ff37ffd0ceaf2f3ebf7ff7",
+    "url": "/simple-react-homeworks-master/static/media/preloader.0ad6a0ce.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks-master/precache-manifest.24b4e74d010aef310908c7155daa1db2.js"
+  "/simple-react-homeworks-master/precache-manifest.f26eb1f4160078adaf366ab1d7250581.js"
 );
 
 self.addEventListener('message', (event) => {
